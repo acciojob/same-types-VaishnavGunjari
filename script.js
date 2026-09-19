8894a62ea07438f232edda19fb3c1af55bf9dad2
Vaishnav <vaishnavgunjari098@gmail.com>
2026-09-20 02:20:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function isSameType(value1, value2) {
   //your js code here
 	if (Number.isNaN(value1) && Number.isNaN(value2)){
 		return true;
-	}if(Number.isNan(value1) || Number.isNaN(value2)){
+	}if(Number.isNaN(value1) || Number.isNaN(value2)){
 		return false;
 	}return typeof value1 === typeof value2;
 }
